68f168cbab74717bb2a38900ad5cf25e50fc4b53
Aadvik Paliwal <aadvikpaliwal2005@gmail.com>
2026-04-21 12:17:29 +0530

    fix csv separator issue

--- project-01-ecommerce/src/extract.py
+++ project-01-ecommerce/src/extract.py
@@ -8,7 +8,7 @@ def load_data():
     orders = pd.read_csv(DATA_PATH + "olist_orders_dataset.csv")
     order_items = pd.read_csv(DATA_PATH + "olist_order_items_dataset.csv")
     order_payments = pd.read_csv(DATA_PATH + "olist_order_payments_dataset.csv")
-    order_reviews = pd.read_csv(DATA_PATH + "olist_order_reviews_dataset.csv", sep=';')
+    order_reviews = pd.read_csv(DATA_PATH + "olist_order_reviews_dataset.csv")
     products = pd.read_csv(DATA_PATH + "olist_products_dataset.csv")
     sellers = pd.read_csv(DATA_PATH + "olist_sellers_dataset.csv")
     geolocation = pd.read_csv(DATA_PATH + "olist_geolocation_dataset.csv")
